8b40215051634736702e20271d6b96dc368c920f
aamir85284 <khan85284@gmail.com>
2021-03-04 19:16:58 +0530

    checkin final

--- src/main/java/com/myapp/spring/rest/api/ProductApi.java
+++ src/main/java/com/myapp/spring/rest/api/ProductApi.java
@@ -3,6 +3,7 @@ package com.myapp.spring.rest.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,8 @@ public class ProductApi {
 	private PassengerRepository repository;
 	// At runtime, it will search for the ProductRepository type object i.e, all
 	// classes implementing this interface
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@GetMapping
 	public List<Product> getAll() {
@@ -52,23 +55,42 @@ public class ProductApi {
 //		return repository.findById(productid);
 //
 //	}
-	@GetMapping("/{id}")
+	@GetMapping("/checkin/{id}")
 	public String findById(@PathVariable("id") Integer productid) {
 
 		// Product existingProduct =repository.findById(productid);
-		if (productid == 1) {
+		// List flightIdList = [1, 2];
 
-			return "<html><body>" + "<h1>Checkin </h1><br> <h3>Your checin done</h3>" + "</body></html>";
-		} else {
-			// return Integer.toString(productid);
-			return "<html><body>" + "<h1>Checkin </h1><br> <h3>Your checin not done</h3>" + "</body></html>";
+		try {
+			String query = "select flight_id from abc_teamd where flight_id=?";
+			Object[] inputs = new Object[] { productid };
+			Integer empName = jdbcTemplate.queryForObject(query, Integer.class, inputs);
+			if (productid == empName) {
+				// return Integer.toString(empName);
+				return "<html><body>" + "<h1>Checkin Details</h1><br> <h3>Your BookingID :- " + empName
+						+ " is present in our database.<br>Congratulations your checkIn is DONE!</h3>"
+						+ "</body></html>";
+			} else {
+				// return Integer.toString(productid);
+				return "<html><body>" + "<h1>Checkin </h1><br> <h3>Your checin not done</h3>" + "</body></html>";
+			}
+		} catch (Exception e) {
+			return "<html><body>"
+					+ "<h1>Checkin Details</h1><br> <h3>Your BookingID is not present in our database.<br>Unfortunatly your checkIn is NOT DONE</h3>"
+					+ "</body></html>";
 		}
+
 	}
 
 	@GetMapping("/checkin")
 	public String welcome() {
 		return "<html><body>" + "<h1>WELCOME TO ABC AIRLINES </h1><br> <h3>We do check in for abc airlines</h3>"
 				+ "</body></html>";
+	}
+
+	@GetMapping("/error")
+	public String error() {
+		return "<html><body>" + "<h1>Checkin </h1><br> <h3>Your checin not done</h3>" + "</body></html>";
 	}
 
 }
