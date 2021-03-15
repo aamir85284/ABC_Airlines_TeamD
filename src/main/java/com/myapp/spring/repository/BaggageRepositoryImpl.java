b764fc949d16a4661d4aedee1bec61898673a643
aamir85284 <khan85284@gmail.com>
2021-03-15 15:46:25 +0530

    Create BaggageRepositoryImpl.java

---
+++ src/main/java/com/myapp/spring/repository/BaggageRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.myapp.spring.repository;
+
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.myapp.spring.model.BaggagePassenger;
+
+@Repository // It's a data Repository Class
+public class BaggageRepositoryImpl implements BaggageRepository {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Override
+	public BaggagePassenger saveProduct(BaggagePassenger product, Integer count) {
+		// TODO Auto-generated method stub
+		// return null;
+		Random rand = new Random();
+		for(int i=0;i<count;i++) {
+			product.addBaggageid(rand.nextInt(100));
+		}
+
+		if(product.getBaggages().contains("null, ")) {
+			product.setBaggages(product.getBaggages().replace("null, ", ""));
+    	}
+		jdbcTemplate.update("insert into abc_baggage values (?,?,?,?)", product.getFlight_id(), product.getBaggages(), 
+				product.getPassenger_name(), product.getPassengerid());
+		return product;
+	}
+
+}
