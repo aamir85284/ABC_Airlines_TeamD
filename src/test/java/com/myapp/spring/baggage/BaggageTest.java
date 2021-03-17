d612ecc4d98f589e62c2a692aa4958557c507972
Rajagiri Sai Pooja <34917414+saipooja123@users.noreply.github.com>
2021-03-17 16:41:35 +0530

    Update BaggageTest.java

--- src/test/java/com/myapp/spring/baggage/BaggageTest.java
+++ src/test/java/com/myapp/spring/baggage/BaggageTest.java
@@ -57,7 +57,7 @@ public class BaggageTest {
 						+ " </h1><br> <h3>Your Baggage id :- " + getUserInfo().getBaggages() + "</body></html>"));
 
 		System.out.println(result.getResponse().getContentAsString());
-		assertFalse(result.getResponse().getContentAsString().contains("<html><body>"
+		assertTrue(result.getResponse().getContentAsString().contains("<html><body>"
 				+ "<h1>Baggage Details</h1><br> <h3>Your BookingID is not present in our database.<br>Unfortunatly your checkIn is NOT DONE</h3>"
 				+ "</body></html>"));
 
