59aa676773d092b41615487e8a840697d19ec350
aamir85284 <khan85284@gmail.com>
2021-03-18 15:33:48 +0530

    Final Integration and implemetation

--- src/main/java/com/myapp/spring/ServletInitializer.java
+++ src/main/java/com/myapp/spring/ServletInitializer.java
@@ -1,13 +1,10 @@
 package com.myapp.spring;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+public class ServletInitializer /* extends SpringBootServletInitializer */ {
 
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AbcAirlinesTeamDApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(AbcAirlinesTeamDApplication.class);
+//	}
 
 }
